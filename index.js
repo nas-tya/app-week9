b979e4a5a5e515984a57013e94578708b42f53a9
nas-tya <60651697+nas-tya@users.noreply.github.com>
2021-05-12 21:34:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,16 @@ app
     res.send('brazhkinanastya01');
 })
 .get('/test/', async (req, res) => {
-    const browser = await puppeteer.launch({args: ['--no-sandbox']});
+    const browser = await puppeteer.launch({args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage,
+        '--disable-accelerated-2d-canvas,
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process',
+        '--disable-gpu'
+    ]});
     const page = await browser.newPage();
     await page.goto(req.query.URL);
     await page.click('#bt');
